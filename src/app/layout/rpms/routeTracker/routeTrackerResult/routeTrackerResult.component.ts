4dbaa0f243c54645fc615961e0cb67498db1cb7b
calhuang <qinjiahu@usc.edu>
2018-06-15 17:50:53 -0700

    modified routeTrackerResult.component

--- src/app/layout/rpms/routeTracker/routeTrackerResult/routeTrackerResult.component.ts
+++ src/app/layout/rpms/routeTracker/routeTrackerResult/routeTrackerResult.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { routerTransition } from '../../../../router.animations';
 import { switchTableService } from './../../../Service/switchTable.service'
-import { nxapiService} from './../../../Service/nxapi.service'
+import { nxapiService } from './../../../Service/nxapi.service'
 import { ElasticsearchService } from './../../../Service/elasticsearch.service'
 
 @Component({
@@ -18,47 +18,66 @@ export class RouteTrackerResultComponent implements OnInit {
         key: "none",
         value: "none"
     }]
+    switchLogs = []
     public isCollapsed = false;
     constructor(private st: switchTableService,
-              private es: ElasticsearchService) {
+        private es: ElasticsearchService) {
     }
-
+    data = {}
     ngOnInit() {
-      let self = this;
-       setInterval(() => {
 
-          this.es.search({
-              index: 'routetracker_stats_*',
-              body: {
-                  "size": 1,
-                  "sort": {
-                      "timestamp": {
-                          "order": "desc"
-                      }
-                  }
-              }
-          }).then(function(resp) {
-              // console.log(resp)
-              // console.log(self.ESresult)
-              self.ESresult = []
-              if (resp.hits.hits[0]._source) {
-                  for (let key in resp.hits.hits[0]._source) {
-                      self.ESresult.push({
-                          key: key,
-                          value: resp.hits.hits[0]._source[key]
-                      })
-                  }
-              }
+        setInterval(() => {
+
+            this.data = {
+                index: 'routetracker_tm_vrf_stats_*',
+                body: {
+                    query: {
+                        "bool": {
+                            "must": [
+                                
+
+                                {
+                                    "match": {
+                                        "prefix": '10.1.3.97/32'
+                                    }
+                                }
+                            ]
+                        }
+
+                    }
+                }
+            }
+            this.searchForSearch(this.data)
 
 
-          }, function(err) {
-              console.trace(err.message);
-          });
-      }, 1000);
+        }, 1000);
+    }
+    onSearch(prefix, owner, vrfname) {
+        // this.data = {
+        //   index :
+        // }
     }
 
+    searchForSearch(data) {
+        let self = this;
+        this.es.search(data).then(function(resp) {
+            console.log(resp)
+            // console.log(self.ESresult)
+            // self.ESresult = []
+            // if (resp.hits.hits[0]._source) {
+            //     for (let key in resp.hits.hits[0]._source) {
+            //         self.ESresult.push({
+            //             key: key,
+            //             value: resp.hits.hits[0]._source[key]
+            //         })
+            //     }
+            // }
 
 
+        }, function(err) {
+            console.trace(err.message);
+        });
 
+    }
 
 }
